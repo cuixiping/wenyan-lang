4131c05e166467991849b203db15b549ac45ecdf
cuixiping <cuixiping@gmail.com>
2019-12-28 22:31:29 +0800

    update demo

--- my-demo/畫布試用.js
+++ my-demo/畫布試用.js
@@ -12,6 +12,7 @@ var 畫布經 = new (function() {
     var 磅 = (this.磅 = "pt")
     var 像素 = (this.像素 = "px")
     var 無襯線 = (this.無襯線 = "sans-serif")
+    var 平常 = (this.平常 = "normal")
     var 文檔 = (this.文檔 = "document")
     var 取元素于名 = (this.取元素于名 = "getElementById")
     var 填充式樣 = (this.填充式樣 = "fillStyle")
@@ -48,6 +49,8 @@ var 畫布經 = new (function() {
   var 推敲 = "click"
   var 按下 = "mousedown"
   var 抬起 = "mouseup"
+  var 空格 = " "
+  var 斜線 = "/"
   var 上下文 = 0
   var 畫布元素 = 0
   var 得一元素 = () => 0
@@ -137,60 +140,62 @@ var 畫布經 = new (function() {
   var 字尺寸 = "12pt"
   var 字行高 = "120%"
   var 字體 = "sans-serif"
+  var 字重 = "normal"
+  var 刷新字之式樣 = () => 0
+  刷新字之式樣 = function() {
+    var _ans11 = 字重 + 空格
+    var _ans12 = _ans11 + 字尺寸
+    var _ans13 = _ans12 + 斜線
+    var _ans14 = _ans13 + 字行高
+    var _ans15 = _ans14 + 空格
+    var _ans16 = _ans15 + 字體
+    var 式樣 = _ans16
+    上下文["font"] = 式樣
+  }
   var 設字尺寸 = () => 0
   設字尺寸 = function(尺寸) {
     字尺寸 = 尺寸
-    var _ans11 = 字尺寸 + "/"
-    var _ans12 = _ans11 + 字行高
-    var _ans13 = _ans12 + " "
-    var _ans14 = _ans13 + 字體
-    var 式樣 = _ans14
-    上下文["font"] = 式樣
+    var _ans17 = 刷新字之式樣()
   }
   var 設字行高 = () => 0
   設字行高 = function(行高) {
     字行高 = 行高
-    var _ans15 = 字尺寸 + "/"
-    var _ans16 = _ans15 + 字行高
-    var _ans17 = _ans16 + " "
-    var _ans18 = _ans17 + 字體
-    var 式樣 = _ans18
-    上下文["font"] = 式樣
+    var _ans18 = 刷新字之式樣()
   }
   var 設字體 = () => 0
   設字體 = function(名號) {
     字體 = 名號
-    var _ans19 = 字尺寸 + "/"
-    var _ans20 = _ans19 + 字行高
-    var _ans21 = _ans20 + " "
-    var _ans22 = _ans21 + 字體
-    var 式樣 = _ans22
-    上下文["font"] = 式樣
+    var _ans19 = 刷新字之式樣()
+  }
+  var 設字重 = () => 0
+  設字重 = function(新字重) {
+    字重 = 新字重
+    var _ans20 = 刷新字之式樣()
   }
   var 監聽術 = () => 0
   監聽術 = function(物) {
     return function(所為) {
       return function(探子) {
-        var _ans23 = 物.addEventListener(所為, 探子)
+        var _ans21 = 物.addEventListener(所為, 探子)
       }
     }
   }
   var 解鼠 = () => 0
   解鼠 = function(鼠) {
-    var _ans24 = 鼠["clientX"]
-    var 橫 = _ans24
-    var _ans25 = 鼠["clientY"]
-    var 縱 = _ans25
-    var _ans26 = 鼠["type"]
-    var 所為 = _ans26
-    var _ans27 = 鼠["button"]
-    var 所按 = _ans27
-    var _ans28 = 畫布元素.getBoundingClientRect()
-    var 畫布方位 = _ans28
-    var _ans29 = 橫 - 畫布方位.left
-    橫 = _ans29
-    var _ans30 = 縱 - 畫布方位.top
-    縱 = _ans30
+    var _ans22 = 鼠["clientX"]
+    var 橫 = _ans22
+    var _ans23 = 鼠["clientY"]
+    var 縱 = _ans23
+    var _ans24 = 鼠["type"]
+    var 所為 = _ans24
+    var _ans25 = 鼠["button"]
+    var 所按 = _ans25
+    var _ans26 = 畫布元素.getBoundingClientRect()
+    var 畫布方位 = _ans26
+    var _ans27 = 橫 - 畫布方位.left
+    橫 = _ans27
+    var _ans28 = 縱 - 畫布方位.top
+    縱 = _ans28
     var 報 = {}
     報 = {
       橫: 橫,
@@ -235,23 +240,23 @@ var 畫布經 = new (function() {
     var 可為 = []
     可為.push(奔走, 推敲, 按下, 抬起)
     for (var 為 of 可為) {
-      var _ans31 = 監聽術(畫布元素, 為, 捕鼠者)
+      var _ans29 = 監聽術(畫布元素)(為)(捕鼠者)
     }
   }
   var 無術 = () => 0
   var 得一畫布 = (this.得一畫布 = () => 0)
   得一畫布 = this.得一畫布 = function(畫布之名) {
-    var _ans32 = 得一元素(畫布之名)
-    畫布元素 = _ans32
+    var _ans30 = 得一元素(畫布之名)
+    畫布元素 = _ans30
     if (畫布元素 == null) {
       var _rand1 = new Error()
       _rand1.name = "無此畫布"
       throw _rand1
       return
     }
-    var _ans33 = 畫布元素.getContext("2d")
-    上下文 = _ans33
-    var _ans34 = 設字尺寸(四號)
+    var _ans31 = 畫布元素.getContext("2d")
+    上下文 = _ans31
+    var _ans32 = 設字尺寸(四號)
     var 畫布 = {}
     畫布 = {
       元素: 畫布元素,
@@ -271,6 +276,7 @@ var 畫布經 = new (function() {
       設字尺寸: 設字尺寸,
       設字行高: 設字行高,
       設字體: 設字體,
+      設字重: 設字重,
       捕鼠者: 無術
     }
     var 捕鼠者 = () => 0
@@ -278,16 +284,16 @@ var 畫布經 = new (function() {
       if (畫布["捕鼠者"] == 無術) {
         return
       }
-      var _ans35 = 畫布["捕鼠者"]
-      var 真捕鼠者 = _ans35
+      var _ans33 = 畫布["捕鼠者"]
+      var 真捕鼠者 = _ans33
       if (真捕鼠者) {
-        var _ans36 = 解鼠(鼠)
-        var 鼠況 = _ans36
-        var _ans37 = 鼠.preventDefault()
-        var _ans38 = 真捕鼠者(鼠況)
+        var _ans34 = 解鼠(鼠)
+        var 鼠況 = _ans34
+        var _ans35 = 鼠.preventDefault()
+        var _ans36 = 真捕鼠者(鼠況)
       }
     }
-    var _ans39 = 捕鼠(捕鼠者)
+    var _ans37 = 捕鼠(捕鼠者)
     return 畫布
   }
   var 字號 = (this.字號 = {})
@@ -342,6 +348,7 @@ var 洋經 = new (function() {
   var 磅 = (this.磅 = "pt")
   var 像素 = (this.像素 = "px")
   var 無襯線 = (this.無襯線 = "sans-serif")
+  var 平常 = (this.平常 = "normal")
   var 文檔 = (this.文檔 = "document")
   var 取元素于名 = (this.取元素于名 = "getElementById")
   var 填充式樣 = (this.填充式樣 = "fillStyle")
@@ -431,6 +438,7 @@ var _ans8 = 畫布.設字尺寸(字號.四號)
 var _ans9 = 畫布.設輪廓之色(深灰色)
 var _ans10 = 畫布.設填充之色(淺灰色)
 var _ans11 = 畫布.設輪廓粗細(2)
+var _ans12 = 畫布.設字重("bold")
 /*畫布.移筆*/
 
 /*畫布.直行*/
@@ -460,34 +468,34 @@ var 製一棋盤 = () => 0
 
   /*縱*/
   for (var _rand1 = 0; _rand1 < 15; _rand1++) {
-    var _ans12 = 畫布.移筆(橫)(縱)
-    var _ans13 = 畫布.直行(右)(縱)
-    var _ans14 = 縱 + 一格
-    縱 = _ans14
+    var _ans13 = 畫布.移筆(橫)(縱)
+    var _ans14 = 畫布.直行(右)(縱)
+    var _ans15 = 縱 + 一格
+    縱 = _ans15
   }
   橫 = 10
   縱 = 10
   for (var _rand2 = 0; _rand2 < 13; _rand2++) {
-    var _ans15 = 畫布.移筆(橫)(縱)
-    var _ans16 = 畫布.直行(橫)(下)
-    var _ans17 = 橫 + 一格
-    橫 = _ans17
+    var _ans16 = 畫布.移筆(橫)(縱)
+    var _ans17 = 畫布.直行(橫)(下)
+    var _ans18 = 橫 + 一格
+    橫 = _ans18
   }
-  var _ans18 = 畫布.描其輪廓()
+  var _ans19 = 畫布.描其輪廓()
 }
 var 製一小格 = () => 0
 製一小格 = function(列序) {
   return function(行序) {
-    var _ans19 = 列序 * 一格
-    var _ans20 = _ans19 + 左
-    var 橫 = _ans20
-    var _ans21 = 行序 * 一格
-    var _ans22 = _ans21 + 上
-    var 縱 = _ans22
-    var _ans23 = 畫布.起筆()
-    var _ans24 = 畫布.矩形(橫)(縱)(一格)(一格)
-    var _ans25 = 畫布.填充其內()
-    var _ans26 = 畫布.描其輪廓()
+    var _ans20 = 列序 * 一格
+    var _ans21 = _ans20 + 左
+    var 橫 = _ans21
+    var _ans22 = 行序 * 一格
+    var _ans23 = _ans22 + 上
+    var 縱 = _ans23
+    var _ans24 = 畫布.起筆()
+    var _ans25 = 畫布.矩形(橫)(縱)(一格)(一格)
+    var _ans26 = 畫布.填充其內()
+    var _ans27 = 畫布.描其輪廓()
   }
 }
 var 數文列 = []
@@ -498,71 +506,75 @@ var 格內題數 = () => 0
 格內題數 = function(數) {
   return function(列序) {
     return function(行序) {
-      var _ans27 = 數文列[數 - 1]
-      var 文 = _ans27
-      var _ans28 = 數彩列[數 - 1]
-      var 彩 = _ans28
-      var _ans29 = 列序 * 一格
-      var _ans30 = _ans29 + 左
-      var _ans31 = _ans30 + 5
-      var 橫 = _ans31
-      var _ans32 = 行序 * 一格
-      var _ans33 = _ans32 + 上
-      var _ans34 = _ans33 + 21
-      var 縱 = _ans34
-      var _ans35 = 畫布.設填充之色(彩)
-      var _ans36 = 畫布.題字(文)(橫)(縱)
+      var _ans28 = 數文列[數 - 1]
+      var 文 = _ans28
+      var _ans29 = 數彩列[數 - 1]
+      var 彩 = _ans29
+      var _ans30 = 列序 * 一格
+      var _ans31 = _ans30 + 左
+      var _ans32 = _ans31 + 5
+      var 橫 = _ans32
+      var _ans33 = 行序 * 一格
+      var _ans34 = _ans33 + 上
+      var _ans35 = _ans34 + 21
+      var 縱 = _ans35
+      var _ans36 = 畫布.設填充之色(彩)
+      var _ans37 = 畫布.題字(文)(橫)(縱)
+      if (數 <= 3) {
+        縱 = 縱 + 1
+        var _ans39 = 畫布.題字(文)(橫)(縱)
+      }
     }
   }
 }
 var 製矩形一開格 = () => 0
 製矩形一開格 = function(列序) {
   return function(行序) {
-    var _ans37 = 列序 * 一格
-    var _ans38 = _ans37 + 左
-    var _ans39 = _ans38 + 0.5
-    var 橫 = _ans39
-    var _ans40 = 行序 * 一格
-    var _ans41 = _ans40 + 上
+    var _ans40 = 列序 * 一格
+    var _ans41 = _ans40 + 左
     var _ans42 = _ans41 + 0.5
-    var 縱 = _ans42
-    var _ans43 = 畫布.設輪廓粗細(1)
-    var _ans44 = 縱 + 一格
-    var _ans45 = _ans44 - 1
-    縱 = _ans45
-    var _ans46 = 畫布.設輪廓之色(暗灰色)
-    var _ans47 = 畫布.起筆()
-    var _ans48 = 畫布.移筆(橫)(縱)
-    var _ans49 = 縱 - 一格
-    var _ans50 = _ans49 + 1
-    縱 = _ans50
-    var _ans51 = 畫布.直行(橫)(縱)
-    var _ans52 = 橫 + 一格
-    var _ans53 = _ans52 - 1
-    橫 = _ans53
+    var 橫 = _ans42
+    var _ans43 = 行序 * 一格
+    var _ans44 = _ans43 + 上
+    var _ans45 = _ans44 + 0.5
+    var 縱 = _ans45
+    var _ans46 = 畫布.設輪廓粗細(1)
+    var _ans47 = 縱 + 一格
+    var _ans48 = _ans47 - 1
+    縱 = _ans48
+    var _ans49 = 畫布.設輪廓之色(暗灰色)
+    var _ans50 = 畫布.起筆()
+    var _ans51 = 畫布.移筆(橫)(縱)
+    var _ans52 = 縱 - 一格
+    var _ans53 = _ans52 + 1
+    縱 = _ans53
     var _ans54 = 畫布.直行(橫)(縱)
-    var _ans55 = 畫布.描其輪廓()
-    var _ans56 = 畫布.設輪廓之色(純白色)
-    var _ans57 = 畫布.起筆()
-    var _ans58 = 畫布.移筆(橫)(縱)
-    var _ans59 = 縱 + 一格
-    var _ans60 = _ans59 - 1
-    縱 = _ans60
-    var _ans61 = 畫布.直行(橫)(縱)
-    var _ans62 = 橫 - 一格
-    var _ans63 = _ans62 + 1
-    橫 = _ans63
+    var _ans55 = 橫 + 一格
+    var _ans56 = _ans55 - 1
+    橫 = _ans56
+    var _ans57 = 畫布.直行(橫)(縱)
+    var _ans58 = 畫布.描其輪廓()
+    var _ans59 = 畫布.設輪廓之色(純白色)
+    var _ans60 = 畫布.起筆()
+    var _ans61 = 畫布.移筆(橫)(縱)
+    var _ans62 = 縱 + 一格
+    var _ans63 = _ans62 - 1
+    縱 = _ans63
     var _ans64 = 畫布.直行(橫)(縱)
-    var _ans65 = 畫布.描其輪廓()
-    var _ans66 = 縱 - 一格
-    var _ans67 = _ans66 + 1.5
-    縱 = _ans67
+    var _ans65 = 橫 - 一格
+    var _ans66 = _ans65 + 1
+    橫 = _ans66
+    var _ans67 = 畫布.直行(橫)(縱)
+    var _ans68 = 畫布.描其輪廓()
+    var _ans69 = 縱 - 一格
+    var _ans70 = _ans69 + 1.5
+    縱 = _ans70
     橫 = 橫 + 0.5
-    var _ans69 = 畫布.起筆()
+    var _ans72 = 畫布.起筆()
     var 一內格 = 一格 - 2.5
-    var _ans71 = 畫布.矩形(橫)(縱)(一內格)(一內格)
-    var _ans72 = 畫布.設填充之色(純白色)
-    var _ans73 = 畫布.填充其內()
+    var _ans74 = 畫布.矩形(橫)(縱)(一內格)(一內格)
+    var _ans75 = 畫布.設填充之色(純白色)
+    var _ans76 = 畫布.填充其內()
   }
 }
 var 新製一棋盤 = () => 0
@@ -575,33 +587,33 @@ var 新製一棋盤 = () => 0
   for (var _rand3 = 0; _rand3 < 14; _rand3++) {
     橫 = 左
     for (var _rand4 = 0; _rand4 < 10; _rand4++) {
-      var _ans74 = 畫布.矩形(橫)(縱)(一格)(一格)
+      var _ans77 = 畫布.矩形(橫)(縱)(一格)(一格)
       橫 = 橫 + 一格
       格棧.push(0)
     }
     縱 = 縱 + 一格
   }
-  var _ans77 = 畫布.填充其內()
-  var _ans78 = 畫布.描其輪廓()
+  var _ans80 = 畫布.填充其內()
+  var _ans81 = 畫布.描其輪廓()
 }
 var 捕鼠者 = () => 0
 捕鼠者 = function(鼠) {
-  var _ans79 = 鼠["橫"]
-  var _ans80 = _ans79 - 左
-  var _ans81 = _ans80 / 一格
-  var _ans82 = 取底(_ans81)
-  var 列序 = _ans82
-  var _ans83 = 鼠["縱"]
-  var _ans84 = _ans83 - 上
-  var _ans85 = _ans84 / 一格
-  var _ans86 = 取底(_ans85)
-  var 行序 = _ans86
+  var _ans82 = 鼠["橫"]
+  var _ans83 = _ans82 - 左
+  var _ans84 = _ans83 / 一格
+  var _ans85 = 取底(_ans84)
+  var 列序 = _ans85
+  var _ans86 = 鼠["縱"]
+  var _ans87 = _ans86 - 上
+  var _ans88 = _ans87 / 一格
+  var _ans89 = 取底(_ans88)
+  var 行序 = _ans89
   if (舊行序 > -1) {
-    var _ans87 = 畫布.起筆()
-    var _ans88 = 畫布.設輪廓之色(深灰色)
-    var _ans89 = 畫布.設填充之色(淺灰色)
-    var _ans90 = 畫布.設輪廓粗細(2)
-    var _ans91 = 製一小格(舊列序)(舊行序)
+    var _ans90 = 畫布.起筆()
+    var _ans91 = 畫布.設輪廓之色(深灰色)
+    var _ans92 = 畫布.設填充之色(淺灰色)
+    var _ans93 = 畫布.設輪廓粗細(2)
+    var _ans94 = 製一小格(舊列序)(舊行序)
   }
   if (行序 < 0) {
     return
@@ -615,27 +627,27 @@ var 捕鼠者 = () => 0
   if (列序 > 9) {
     return
   }
-  var _ans92 = 行序 * 10
-  var _ans93 = _ans92 + 列序
-  var _ans94 = _ans93 + 1
-  var 棧序 = _ans94
+  var _ans95 = 行序 * 10
+  var _ans96 = _ans95 + 列序
+  var _ans97 = _ans96 + 1
+  var 棧序 = _ans97
   var 棧值 = 格棧[棧序 - 1]
   if (棧值 > 0) {
     return
   }
-  if (鼠[奔走乎 - 1]) {
-    var _ans96 = 畫布.起筆()
-    var _ans97 = 畫布.設輪廓之色(深灰色)
-    var _ans98 = 畫布.設填充之色(白煙色)
-    var _ans99 = 畫布.設輪廓粗細(2)
-    var _ans100 = 製一小格(列序)(行序)
+  if (鼠["奔走乎"]) {
+    var _ans99 = 畫布.起筆()
+    var _ans100 = 畫布.設輪廓之色(深灰色)
+    var _ans101 = 畫布.設填充之色(白煙色)
+    var _ans102 = 畫布.設輪廓粗細(2)
+    var _ans103 = 製一小格(列序)(行序)
     舊行序 = 行序
     舊列序 = 列序
     return
   }
-  if (鼠[推敲乎 - 1]) {
-    var _ans101 = 製矩形一開格(列序)(行序)
-    var _ans102 = 格內題數(臨時之數)(列序)(行序)
+  if (鼠["推敲乎"]) {
+    var _ans104 = 製矩形一開格(列序)(行序)
+    var _ans105 = 格內題數(臨時之數)(列序)(行序)
     臨時之數 = 臨時之數 + 1
     if (臨時之數 > 8) {
       臨時之數 = 1
@@ -649,5 +661,5 @@ var 捕鼠者 = () => 0
 var 舊行序 = -1
 var 舊列序 = -1
 var 臨時之數 = 1
-var _ans104 = 新製一棋盤()
+var _ans107 = 新製一棋盤()
 畫布["捕鼠者"] = 捕鼠者
